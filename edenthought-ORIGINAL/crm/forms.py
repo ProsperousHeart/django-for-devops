5e3299f7626b278ef4cbb3a6d4908075bdc57c27
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2025-07-07 20:58:39 -0500

    removing original instructor's files

--- edenthought-ORIGINAL/crm/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import DailyNote
-
-
-class DailyNoteForm(forms.ModelForm):
-
-    class Meta:
-        model = DailyNote
-        fields = ["title"]
--- edenthought-ORIGINAL/static/js/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('note-form').onsubmit = function(event){
-
-    event.preventDefault();
-
-    alert("Your note was added!");
-
-    this.submit();
-
-}
